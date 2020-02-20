963b352147d1d06c505d18e9169e7899f9a07723
mayullap <mayullap@gmail.com>
2020-02-20 21:39:20 +0800

    del checkpoint

--- src/cryptonote_basic/checkpoints.cpp
+++ src/cryptonote_basic/checkpoints.cpp
@@ -136,21 +136,11 @@ namespace cryptonote
   }
 
   bool checkpoints::init_default_checkpoints()
-  {
-    ADD_CHECKPOINT(1,     "6044dcbb7fdbf3615ec4d618c667ac8f0ee0082cda621d00e8ccc577ab915749");
-    ADD_CHECKPOINT(10,    "fa81ba4e9649e577eb8afa5bb52b025a5cdf7857b199e0e52b51f5ec44d4caf1");
-    ADD_CHECKPOINT(100,   "dd7fe42eb8b5824e95bf95c091bbeea2367f1e3fcb21b30f7a087872f5540795");
-    ADD_CHECKPOINT(1000,  "95e016693e0422dbfec51e74342bebcad7b7684f04040c559b9284f937ad1d32");
-    ADD_CHECKPOINT(10000, "9c4b950f0439d9beb1051d6e39374291300d8cc4c3223aa8d432c04e291b591a");
-    ADD_CHECKPOINT(25000, "965d533cef5baab75424a58748eada049b5af6ec587fab619810fe38f43e6ed6");
-    ADD_CHECKPOINT(50000, "8c518cc2740c8678b9e685dc9a159f966899ec9c2ee49c3f229e3f5464dd5e1e");
-    ADD_CHECKPOINT(56000, "0a647610abfad692ee81b2a33e6c2958c7d298e9fd1a5c02526209b341f92d1f");
-    ADD_CHECKPOINT(76501, "be1ccf38a7a9f5509303f594c65192b4cb611e38507c0aca1810f38e55cfd0a1");
-    ADD_CHECKPOINT(100000, "52648fe3c9b2485c0823652d46c9900fa9593e10db75cac6ba3f13adc9d78ec8");    
-	ADD_CHECKPOINT(110000, "099d1185f1fdd5f0023400bf5b868782fbd0c4d7362fc906e577af2415378dfd");
-
+  { 
     return true;
   }
+
+  //del all checkpoints
 
   bool checkpoints::load_checkpoints_from_json(const std::string json_hashfile_fullpath)
   {
@@ -188,7 +178,7 @@ namespace cryptonote
   {
     std::vector<std::string> records;
 
-    // CHANGEME add LTHN DNS checkpoints
+	// Changeme VPNC checkpoints
     static const std::vector<std::string> dns_urls = {
     };
 
